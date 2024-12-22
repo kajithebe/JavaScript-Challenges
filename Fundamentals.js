9882d4c4a5d0ed7349550e07efe5fb0dd202f101
Kaji Thebe <kajithebe2000@gmail.com>
2024-12-22 22:31:44 +0200

    Added challenges

--- Fundamentals.js
+++ Fundamentals.js
@@ -68,3 +68,50 @@ function expandedForm(num) {
 }
 
 console.log(expandedForm(70304));
+
+/*
+Count the number of divisors of a positive integer n.
+
+Examples (input --> output)
+4 --> 3 // we have 3 divisors - 1, 2 and 4
+5 --> 2 // we have 2 divisors - 1 and 5
+12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+*/
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(getDivisorsCnt(1200000));
+
+//Given an array of integers, return a new array with each value doubled.
+function maps(x) {
+  return x.map((num) => num * 2);
+}
+
+console.log(maps([1, 2, 3]));
+
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+function solution(string) {
+  return string
+    .split("")
+    .map((char) => (char === char.toUpperCase() ? ` ${char}` : char))
+    .join("");
+}
+
+console.log(solution("camelCasing"));
